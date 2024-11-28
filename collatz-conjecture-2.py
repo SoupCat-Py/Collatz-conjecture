59ae531cddf321d4e77abcbc867176038c14f4a5
Soup Cat <daniel.breuls@gmail.com>
2024-11-28 10:19:28 -0700

    remade the script to be more compact

---
+++ collatz-conjecture-2.py
@@ -0,0 +1,49 @@
+import random
+
+def start(style):
+    while True:
+        numList = []
+        asking = True
+        while asking:
+            if style == 'input':
+                number = input('enter any number ')
+            elif style == 'random':
+                number = input('enter maximum value ')
+                
+            try:
+                number = int(number)
+                asking = False
+            except ValueError:
+                print('invalid input ')
+                asking = True
+
+            if style == 'random':
+                number = random.randint(1,number)
+
+            print(f'''chosen number: {number}
+''')
+            
+        while number != 1:
+            if '.5' not in str(number / 2):
+                number = number / 2
+            else:
+                number = number * 3 + 1
+            print(round(number))
+            numList.append(number)
+        numList.sort()
+        print(f'''reached loop
+terms until 4-2-1 loop: {len(numList) - 3}
+largest number: {round(numList[len(numList)-1])}
+''')
+
+asking = True
+while asking:
+    mode = input('input or random? ')
+    if mode.lower() == 'random':
+        asking = False
+        start('random')
+    elif mode.lower() == 'input':
+        asking = False
+        start('input')
+    else:
+        print('invalid input ')
